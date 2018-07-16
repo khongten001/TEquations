e60bd91a5d988d3d37abfb061a984b5608efea87
Alberto <2848696+albertodev01@users.noreply.github.com>
2018-07-16 14:49:02 +0200

    Update Fraction.pas

--- Delphi/Source/Fraction.pas
+++ Delphi/Source/Fraction.pas
@@ -7,28 +7,30 @@ uses
 
 type
  TFraction = record
-  strict private
-   aNumerator: integer;
-   aDenominator: integer;
-   function GCD(a, b: integer): integer;
-   function FromString(const s: string): TFraction;
-   function FromDouble(const d: double): TFraction;
-   function GetDecimal: double;
-   function GetString: string;
-  public
-   constructor Create(aNumerator: integer; aDenominator: integer); overload;
-   constructor Create(aFraction: string); overload;
-   constructor Create(aFraction: double); overload;
-   procedure Reduce;
-   class operator Add(fraction1, fraction2: TFraction): TFraction;
-   class operator Subtract(fraction1, fraction2: TFraction): TFraction;
-   class operator Multiply(fraction1, fraction2: TFraction): TFraction;
-   class operator Divide(fraction1, fraction2: TFraction): TFraction;
-   class operator Negative(const Value: TFraction): TFraction;
-   property Numerator: integer read aNumerator;
-   property Denominator: integer read aDenominator;
-   property ToDouble: double read GetDecimal;
-   property ToString: string read GetString;
+   strict private
+     aNumerator: integer;
+     aDenominator: integer;
+     function GCD(a, b: integer): integer;
+     function FromString(const s: string): TFraction;
+     function FromDouble(const d: double): TFraction;
+     function GetDecimal: double;
+     function GetString: string;
+   public
+     constructor Create(aNumerator: integer; aDenominator: integer); overload;
+     constructor Create(aFraction: string); overload;
+     constructor Create(aFraction: double); overload;
+     procedure Reduce;
+     procedure Negate;
+     procedure Inverse;
+     class operator Add(fraction1, fraction2: TFraction): TFraction;
+     class operator Subtract(fraction1, fraction2: TFraction): TFraction;
+     class operator Multiply(fraction1, fraction2: TFraction): TFraction;
+     class operator Divide(fraction1, fraction2: TFraction): TFraction;
+     class operator Negative(const Value: TFraction): TFraction;
+     property Numerator: integer read aNumerator;
+     property Denominator: integer read aDenominator;
+     property ToDouble: double read GetDecimal;
+     property ToString: string read GetString;
  end;
 
 implementation
@@ -67,12 +69,12 @@ end;
 function TFraction.GCD(a, b: integer): integer;
 var remd: integer;
 begin
- remd := a mod b;
+  remd := a mod b;
 
- if remd = 0 then
-  Result := b
- else
-  Result := gcd(b, remd);
+  if remd = 0 then
+   Result := b
+  else
+   Result := gcd(b, remd);
 end;
 
 function TFraction.GetDecimal: double;
@@ -85,34 +87,45 @@ begin
   Result := aNumerator.ToString + '/' + aDenominator.ToString;
 end;
 
+procedure TFraction.Inverse;
+var temp: integer;
+begin
+  temp := aNumerator;
+  aNumerator := aDenominator;
+  aDenominator := temp;
+end;
+
 procedure TFraction.Reduce;
 var LGCD: integer;
 begin
- LGCD := GCD(aNumerator, aDenominator);
- aNumerator := aNumerator div LGCD;
- aDenominator := aDenominator div LGCD;
+  LGCD := GCD(aNumerator, aDenominator);
+  aNumerator := aNumerator div LGCD;
+  aDenominator := aDenominator div LGCD;
 end;
 
 //operators overloads
 
 class operator TFraction.Add(fraction1, fraction2: TFraction): TFraction;
 begin
- Result := TFraction.Create(fraction1.Numerator*fraction2.Denominator+fraction1.Denominator*fraction2.Numerator,fraction1.Denominator*fraction2.Denominator);
+  Result.aNumerator := fraction1.Numerator*fraction2.Denominator+fraction1.Denominator*fraction2.Numerator;
+  Result.aDenominator := fraction1.Denominator*fraction2.Denominator;
 end;
 
 class operator TFraction.Subtract(fraction1, fraction2: TFraction): TFraction;
 begin
- Result := fraction1 + (-fraction2);
+  Result := fraction1 + (-fraction2);
 end;
 
 class operator TFraction.Multiply(fraction1, fraction2: TFraction): TFraction;
 begin
- Result := TFraction.Create(fraction1.Numerator*fraction2.Numerator,fraction1.Denominator*fraction2.Denominator);
+  Result.aNumerator := fraction1.Numerator*fraction2.Numerator;
+  Result.aDenominator := fraction1.Denominator*fraction2.Denominator;
 end;
 
 class operator TFraction.Divide(fraction1, fraction2: TFraction): TFraction;
 begin
- Result := TFraction.Create(fraction1.Numerator*fraction2.Denominator,fraction1.Denominator*fraction2.Numerator);
+  Result.aNumerator := fraction1.Numerator*fraction2.Denominator;
+  Result.aDenominator := fraction1.Denominator*fraction2.Numerator;
 end;
 
 function TFraction.FromDouble(const d: double): TFraction;
@@ -156,26 +169,31 @@ end;
 
 function TFraction.FromString(const s: string): TFraction;
 var
- BarPos: integer;
- numStr, denomStr: string;
+  BarPos: integer;
+  numStr, denomStr: string;
 begin
- BarPos := Pos('/', S);
- if BarPos = 0 then
-   begin
+  BarPos := Pos('/', S);
+  if BarPos = 0 then
+    begin
      Result.aNumerator := StrToInt(S);
      Result.aDenominator := 1;
-   end;
+    end;
 
- numStr := Trim(Copy(S, 1, BarPos - 1));
- denomStr := Trim(Copy(S, BarPos + 1, Length(S)));
- Result.aNumerator := StrToInt(numStr);
- Result.aDenominator := StrToInt(denomStr);
+  numStr := Trim(Copy(S, 1, BarPos - 1));
+  denomStr := Trim(Copy(S, BarPos + 1, Length(S)));
+  Result.aNumerator := StrToInt(numStr);
+  Result.aDenominator := StrToInt(denomStr);
+end;
+
+procedure TFraction.Negate;
+begin
+  aNumerator := (-1) * aNumerator;
 end;
 
 class operator TFraction.Negative(const Value: TFraction): TFraction;
 begin
- Result := TFraction.Create(-Value.aNumerator, Value.aDenominator);
+  Result.aNumerator := -Value.aNumerator;
+  Result.aDenominator := Value.aDenominator;
 end;
 
 end.
-
