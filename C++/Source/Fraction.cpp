2673ae094a29b8ca9ec4fce0ee7cb8c973dd3c90
Alberto <2848696+albertodev01@users.noreply.github.com>
2018-08-13 01:49:27 +0200

    Update Fraction.cpp

--- C++/Source/Fraction.cpp
+++ C++/Source/Fraction.cpp
@@ -1,217 +1,121 @@
-unit Fraction;
+#include "Fraction.h"
+#include <cmath>
+#include <regex>
 
-interface
+namespace NA_Fraction {
 
-uses
- SysUtils, Math, RegularExpressions;
+	Fraction::Fraction(const std::string& f) {
+		//this regex ensures that the denominator will never be zero. Only integers and fractions are accepted
+		if (!std::regex_match(f, std::regex{ "(?:[1-9][0-9]*|0)(?:\/[1-9][0-9]*)?" }))
+			throw std::runtime_error("Wrong input format");		
 
-type
- TFraction = record
-   strict private
-     aNumerator: integer;
-     aDenominator: integer;
-     function GCD(a, b: integer): integer;
-     function FromString(const s: string): TFraction;
-     function FromDouble(const d: double): TFraction;
-     function GetDecimal: double;
-     function GetString: string;
-   public
-     constructor Create(aNumerator: integer; aDenominator: integer); overload;
-     constructor Create(aFraction: string); overload;
-     constructor Create(aFraction: double); overload;
-     procedure Reduce;
-     procedure Negate;
-     procedure Inverse;
-     class operator Add(fraction1, fraction2: TFraction): TFraction;
-     class operator Subtract(fraction1, fraction2: TFraction): TFraction;
-     class operator Multiply(fraction1, fraction2: TFraction): TFraction;
-     class operator Divide(fraction1, fraction2: TFraction): TFraction;
-     class operator Negative(const Value: TFraction): TFraction;
-     class operator Implicit(fraction: TFraction): double;
-     class operator Implicit(fraction: TFraction): string;
-     property Numerator: integer read aNumerator;
-     property Denominator: integer read aDenominator;
-     property ToDouble: double read GetDecimal;
-     property ToString: string read GetString;
- end;
+		auto barPos = f.find("/");
+		if (barPos == std::string::npos) {
+			numerator = std::stoi(f);
+			denominator = 1;
+		}
+		else {
+			numerator = std::stoi(f.substr(0, barPos));
+			denominator = std::stoi(f.substr(barPos + 1, f.length()));
+		}
+	}
 
-implementation
+	Fraction::Fraction(double x) {
+		int mul = (x >= 0) ? 1 : -1;
+		x = fabs(x);
 
-{ TFraction }
+		double limit = 1.0E-6;
+		double h1 = 1, h2 = 0, k1 = 0, k2 = 1;
+		double y = x;
 
-constructor TFraction.Create(aNumerator, aDenominator: integer);
-begin
-  if (aDenominator = 0) then
-   begin
-    raise Exception.Create('Denominator cannot be zero in rationals!');
-   end;
+		do {
 
-  Self.aNumerator := aNumerator;
-  Self.aDenominator := aDenominator;
-end;
+			double a = floor(y);
+			double aux = h1;
+			h1 = a * h1 + h2;
+			h2 = aux;
+			aux = k1;
+			k1 = a * k1 + k2;
+			k2 = aux;
+			y = 1 / (y - a);
 
-constructor TFraction.Create(aFraction: string);
-var Temp: TFraction;
-    k: double;
-begin
-  if (TryStrToFloat(aFraction, k)) then
-    Temp := FromDouble(k)
-  else
-    Temp := FromString(aFraction);
+		} while (fabs(x - h1 / k1) > x*limit);
 
-  Self.aNumerator := Temp.aNumerator;
-  Self.aDenominator := Temp.aDenominator;
-end;
+		numerator = mul * static_cast<int>(h1);
+		denominator = static_cast<int>(k1);
+	}
 
-constructor TFraction.Create(aFraction: double);
-var Temp: TFraction;
-begin
-  Temp := FromDouble(aFraction);
+	int Fraction::gcd(int a, int b) const {
+		int remd = a % b;
+		return (remd == 0) ? b : gcd(b, remd);
+	}
 
-  Self.aNumerator := Temp.aNumerator;
-  Self.aDenominator := Temp.aDenominator;
-end;
+	void Fraction::Reduce() {
+		int LGCD = gcd(numerator, denominator);
+		numerator = numerator / LGCD;
+		denominator = denominator / LGCD;
+	}
 
-function TFraction.GCD(a, b: integer): integer;
-var remd: integer;
-begin
-  remd := a mod b;
+	void Fraction::Inverse() {
+		std::swap(numerator, denominator);
+	}
 
-  if remd = 0 then
-   Result := b
-  else
-   Result := gcd(b, remd);
-end;
+	void Fraction::Negate() {
+		numerator *= -1;
+	}
 
-function TFraction.GetDecimal: double;
-begin
-  Result := aNumerator / aDenominator;
-end;
+	double Fraction::toDouble() const {
+		return static_cast<double>(numerator) / denominator;
+	}
 
-function TFraction.GetString: string;
-begin
-  Result := aNumerator.ToString + '/' + aDenominator.ToString;
-end;
+	std::string Fraction::toString() const {
+		return std::string(std::to_string(numerator) + "/" + std::to_string(denominator));
+	}
 
-class operator TFraction.Implicit(fraction: TFraction): string;
-begin
-  Result := fraction.Numerator.ToString + '/' + fraction.Denominator.ToString;
-end;
+	Fraction::operator double() const {
+		return toDouble();
+	}
 
-class operator TFraction.Implicit(fraction: TFraction): double;
-begin
-  Result := fraction.Numerator / fraction.Denominator;
-end;
+	Fraction::operator std::string() const {
+		return toString();
+	}
 
-procedure TFraction.Inverse;
-var temp: integer;
-begin
-  temp := aNumerator;
-  aNumerator := aDenominator;
-  aDenominator := temp;
-end;
+	Fraction& Fraction::operator++() {
+		numerator += denominator;
+		return *this;
+	}
 
-procedure TFraction.Reduce;
-var LGCD: integer;
-begin
-  LGCD := GCD(aNumerator, aDenominator);
-  aNumerator := aNumerator div LGCD;
-  aDenominator := aDenominator div LGCD;
-end;
+	Fraction& Fraction::operator--() {
+		numerator -= denominator;
+		return *this;
+	}
 
-//operators overloads
+	Fraction Fraction::operator++(int) {
+		Fraction s(*this);
+		s.numerator += s.denominator;
+		return s;
+	}
 
-class operator TFraction.Add(fraction1, fraction2: TFraction): TFraction;
-begin
-  Result.aNumerator := fraction1.Numerator*fraction2.Denominator+fraction1.Denominator*fraction2.Numerator;
-  Result.aDenominator := fraction1.Denominator*fraction2.Denominator;
-end;
+	Fraction Fraction::operator--(int) {
+		Fraction s(*this);
+		s.numerator -= s.denominator;
+		return s;
+	}
 
-class operator TFraction.Subtract(fraction1, fraction2: TFraction): TFraction;
-begin
-  Result := fraction1 + (-fraction2);
-end;
+	Fraction operator+(const Fraction& fraction1, const Fraction& fraction2) {
+		return Fraction(fraction1.getNumerator()*fraction2.getDenominator() + fraction1.getDenominator()*fraction2.getNumerator(), fraction1.getDenominator() * fraction2.getDenominator());
+	}
 
-class operator TFraction.Multiply(fraction1, fraction2: TFraction): TFraction;
-begin
-  Result.aNumerator := fraction1.Numerator*fraction2.Numerator;
-  Result.aDenominator := fraction1.Denominator*fraction2.Denominator;
-end;
+	Fraction operator-(const Fraction& fraction1, const Fraction& fraction2) {
+		return Fraction(fraction1.getNumerator()*fraction2.getDenominator() - fraction1.getDenominator()*fraction2.getNumerator(), fraction1.getDenominator() * fraction2.getDenominator());
+	}
 
-class operator TFraction.Divide(fraction1, fraction2: TFraction): TFraction;
-begin
-  Result.aNumerator := fraction1.Numerator*fraction2.Denominator;
-  Result.aDenominator := fraction1.Denominator*fraction2.Numerator;
-end;
+	Fraction operator*(const Fraction& fraction1, const Fraction& fraction2) {
+		return Fraction(fraction1.getNumerator()*fraction2.getNumerator(), fraction1.getDenominator() * fraction2.getDenominator());
+	}
 
-function TFraction.FromDouble(const d: double): TFraction;
-var h1, h2, k1, k2, y, a, aux, sign: double;
-begin
-  h1 := 1;
-  h2 := 0;
-  k1 := 0;
-  k2 := 1;
-  y := abs(d);
-  sign := 1;
+	Fraction operator/(const Fraction& fraction1, const Fraction& fraction2) {
+		return Fraction(fraction1.getNumerator()*fraction2.getDenominator(), fraction1.getDenominator() * fraction2.getNumerator());
+	}
 
-  repeat
-    begin
-      a := floor(y);
-      aux := h1;
-      h1 := a * h1 + h2;
-      h2 := aux;
-      aux := k1;
-      k1 := a * k1 + k2;
-      k2 := aux;
-      if (y - a = 0) or (k1 = 0) then break;
-      y := 1 / (y - a) ;
-    end;
-  until (Abs(abs(d) - h1 / k1) <= abs(d) * 0.0000000001);
-
-  if (d < 0) then
-    sign := -1;
-
-  if not(h1 = 0) then
-    begin
-      Result.aNumerator := Round(sign*h1);
-      Result.aDenominator := Round(k1);
-    end
-  else
-    begin
-      Result.aNumerator := 0;
-      Result.aDenominator := 1;
-    end;
-end;
-
-function TFraction.FromString(const s: string): TFraction;
-var
-  BarPos: integer;
-  numStr, denomStr: string;
-begin
-  BarPos := Pos('/', S);
-  if BarPos = 0 then
-    begin
-     Result.aNumerator := StrToInt(S);
-     Result.aDenominator := 1;
-    end
-  else
-    begin
-      numStr := Trim(Copy(S, 1, BarPos - 1));
-      denomStr := Trim(Copy(S, BarPos + 1, Length(S)));
-      Result.aNumerator := StrToInt(numStr);
-      Result.aDenominator := StrToInt(denomStr);
-    end;
-end;
-
-procedure TFraction.Negate;
-begin
-  aNumerator := (-1) * aNumerator;
-end;
-
-class operator TFraction.Negative(const Value: TFraction): TFraction;
-begin
-  Result.aNumerator := -Value.aNumerator;
-  Result.aDenominator := Value.aDenominator;
-end;
-
-end.
+}
