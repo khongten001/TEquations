cd8e6199343ec9b67b97983408a8f4252d24ac35
Alberto <2848696+albertodev01@users.noreply.github.com>
2018-08-27 23:26:58 +0200

    Update Fraction.h

--- C++/Source/Fraction.h
+++ C++/Source/Fraction.h
@@ -2,21 +2,44 @@
 #define FRACTION_h
 
 #include <string>
+#include <optional>
 #include <exception>
 
 namespace NA_Fraction {
 
+	struct MixedFraction {
+	private:
+		int whole;
+		int num, den;
+	public:
+		MixedFraction(int wholePart, int numerator, int denominator) : whole(wholePart), num(numerator), den(denominator) {
+			if (numerator > denominator)
+				throw std::logic_error("Numerator cannot be greater than the denominator.");
+
+			if (denominator == 0)
+				throw std::logic_error("Denominator cannot be zero.");
+		}
+		
+		inline int getWholePart() const { return whole; }
+		inline int getNumerator() const { return num; }
+		inline int getDenominator() const { return den; }
+		std::string toString() const;
+		double toDouble() const;
+	};
+
 	struct Fraction {
 	private:
-		int numerator, denominator;
+		int numerator, denominator; 
 		int gcd(int a, int b) const;
 	public:
 		Fraction(int aNumerator, int aDenominator) : numerator(aNumerator), denominator(aDenominator) {
 			if (aDenominator == 0)
-				throw std::runtime_error("Denominator cannot be zero.");
+				throw std::logic_error("Denominator cannot be zero.");
 		}
 		explicit Fraction(const std::string& f);
 		explicit Fraction(double x);
+		explicit Fraction(const MixedFraction& m) : numerator(m.getWholePart() * m.getDenominator() + m.getNumerator()), 
+													denominator(m.getDenominator()) {};
 
 		void Reduce();
 		void Negate();
@@ -25,6 +48,7 @@ namespace NA_Fraction {
 		std::string toString() const;
 		operator double() const;
 		operator std::string() const;
+		std::optional<MixedFraction> toMixedFraction() const;
 		inline int getNumerator() const { return numerator; }
 		inline int getDenominator() const { return denominator; }
 
